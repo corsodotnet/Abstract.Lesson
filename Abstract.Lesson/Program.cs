8f9eb6a9ec4f01e1c4a6c83292d368cb950ef396
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-22 12:23:31 +0200

    Add project files.

---
+++ Abstract.Lesson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Abstract.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangolo triangolo = new Triangolo();
+            Rettangolo rettangolo = new Rettangolo();
+
+            FiguraGeometrica figuraGeometrica = new Triangolo();
+           // DimmiAreaFiguraGeometrica(figuraGeometrica);
+
+             figuraGeometrica = rettangolo;
+             DimmiAreaFiguraGeometrica(figuraGeometrica);
+        }
+
+        public static void DimmiAreaFiguraGeometrica(FiguraGeometrica figuraGeometrica)
+        {
+            figuraGeometrica.GetPerimentro();
+        }
+    }
+}
