ce4a13db9bbb754d0114812ed71d49ae28f5b1e3
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-23 10:52:11 +0200

    Constructor

---
+++ Constructor.Lesson/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Constructor.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Auto panda = new Auto("white");
+            Auto punto = new Auto("Red");
+            Auto audiA3 = new Auto("Pink");
+            Auto Ferrari = new Auto("black");
+
+            Console.WriteLine(" panda Color: " + panda._color);
+            Console.WriteLine(" punto Color: " + punto._color);
+            Console.WriteLine(" audiA3 Color: " + audiA3._color);
+            Console.WriteLine(" Ferrari Color: " + Ferrari._color);
+        }
+    }
+    public class Auto
+    {
+        public string _color;
+
+        public Auto(string Color)
+        {
+            this._color = Color;
+        }
+
+    }
+}
