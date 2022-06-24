79ebf84e70e5adfeedeb9d6ca0d2621245823df1
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-24 12:12:05 +0200

    struttura delle Classi

---
+++ Classe.Lesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Classe.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+
+            //Auto auto1 = new Auto("FIAT PUNTO");
+            //Auto auto2 = new Auto("AUDI");
+            //Auto auto3 = new Auto("FERRARI");
+            //Auto auto4 = new Auto("FIAT RITMO");
+            //Auto auto5 = new Auto("BMW");   
+
+            //Console.WriteLine(auto1._name);
+            //Console.WriteLine(auto2._name);
+            //Console.WriteLine(auto3._name);
+            //Console.WriteLine(auto4._name);
+            //Console.WriteLine(auto5._name);
+        }
+    }
+
+    public class Auto
+    {
+        public string _name; 
+        public Auto(string NomeAuto)
+        {
+            _name = NomeAuto;   
+        }
+    }
+
+
+    public class Paese
+    {
+
+    }
+}
